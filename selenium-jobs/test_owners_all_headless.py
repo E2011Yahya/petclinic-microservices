2a7c011be46f828f6263c697e248d90ba8c51da2
E2011Yahya <samueldeniz80@gmail.com>
2020-12-19 19:09:01 +0000

    added selenium jobs written in python

--- selenium-jobs/test_owners_all_headless.py
+++ selenium-jobs/test_owners_all_headless.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import random
+from time import sleep
 
 # Set chrome options for working with headless mode (no screen)
 chrome_options = webdriver.ChromeOptions()
@@ -17,30 +16,13 @@ url = "http://ec2-34-236-170-96.compute-1.amazonaws.com:8080"
 driver.get(url)
 owners_link = driver.find_element_by_link_text("OWNERS")
 owners_link.click()
-all_link = driver.find_element_by_link_text("REGISTER")
+all_link = driver.find_element_by_link_text("ALL")
 all_link.click()
 
-# Register new Owner to Petclinic App
-fn_field = driver.find_element_by_name('firstName')
-fn = 'Callahan' + str(random.randint(0, 100))
-fn_field.send_keys(fn)
-fn_field = driver.find_element_by_name('lastName')
-fn_field.send_keys('Clarusway')
-fn_field = driver.find_element_by_name('address')
-fn_field.send_keys('Ridge Corp. Street')
-fn_field = driver.find_element_by_name('city')
-fn_field.send_keys('McLean')
-fn_field = driver.find_element_by_name('telephone')
-fn_field.send_keys('+1230576803')
-fn_field.send_keys(Keys.ENTER)
-
-# Wait until Owner List table loaded
+# Verify that table loaded
+sleep(1)
 verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
-# Verify that new user is added to Owner List
-if fn in driver.page_source:
-    print(fn, 'is added and found in the Owners Table')
-    print("Test Passed")
-else:
-    print(fn, 'is not found in the Owners Table')
-    print("Test Failed")
+
+print("Table loaded")
+
 driver.quit()
